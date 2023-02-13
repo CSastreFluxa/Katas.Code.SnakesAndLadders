c651b54809adba22c6fe4f31fc9963e13ff7e9a5
Cristòfol Sastre <Fdsa3@iberostar.com>
2023-02-13 01:43:55 +0100

    US1 - UAT3 - Test

--- SnakesAndLadders/Test/GameServiceShould.cs
+++ SnakesAndLadders/Test/GameServiceShould.cs
@@ -39,5 +39,26 @@ namespace Test
 
             playerPosition.Should().Be(expectedPosition);
         }
+
+        // US1 UAT3
+        [Fact]
+        public void MovePlayerToPosition8_WhenIsMoved3Spaces_AndThenIsMoved4Spaces_GivenIsOnPosition1()
+        {
+            var game = new GameService();
+
+            game.Initialize(1);
+            game.Players.First().Position = 1;
+
+            var playerId = 1;
+            var positionsToMove = 3;
+            game.Move(playerId, positionsToMove);
+            positionsToMove = 4;
+            game.Move(playerId, positionsToMove);
+
+            var playerPosition = game.Players.First().Position;
+            var expectedPosition = 8;
+
+            playerPosition.Should().Be(expectedPosition);
+        }
     }
 }
